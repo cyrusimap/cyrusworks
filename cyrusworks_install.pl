9bd7fdf5cf16c27a01db2ce27b5a4fae8efb9738
Chris Davies <chris_davies@fastmail.fm>
2016-10-31 10:25:30 +1100

    missing ` and ;

--- cyrusworks_install.pl
+++ cyrusworks_install.pl
@@ -105,8 +105,11 @@ print "\nRetrying plugins that failed to download...";
 `git clone https://github.com/cyrusimap/cyrus-docker.git /cyrusworks/cyrus-docker/`;
 
 #Build cyrus-docker OS images:
-`cd /cyrusworks/cyrus-docker/; make all run`
+`cd /cyrusworks/cyrus-docker/; make all run`;
 
 print "\n\nThe admin password is : $admin_password \n";
+
+#For each container, ... #Start the images, link them to cyrusworks-jenkins
+`docker run -e "COMMIT=cyrus-imapd-2.5" -t -i --link cyrusworks-jenkins debian`;
 
 
